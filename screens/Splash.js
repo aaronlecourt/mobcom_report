c91d6eefb9f5aa867f2b008a966dbb2d1232c434
Aaron Lecourt <20217375@s.ubaguio.edu>
2023-12-03 00:06:08 +0800

    for publish

--- screens/Splash.js
+++ screens/Splash.js
@@ -5,7 +5,7 @@ import { StyleSheet, View, Platform } from 'react-native';
 export default function SplashScreen() {
     return (
       <View style={styles.container}>
-        <LottieView source={require('./animation.json')} autoPlay loop style={{ width: 250, height: 250 }}/>
+        <LottieView source={require('./animation.json')} autoPlay loop style={{ width: 140, height: 140 }}/>
       </View>
     );
   }
